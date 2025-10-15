d986f9452889902b021fbdb3d55a7838cdb13cc3
lamn5 <78042932+lamn5@users.noreply.github.com>
2025-10-15 00:48:20 -0400

    Update main file

--- main.rb
+++ main.rb
@@ -1,15 +1,6 @@
-require_relative "element"   # uncomment to load element.rb
-require_relative "player"    # uncomment to load player.rb
-require_relative "history"   # uncomment to load history.rb
-
-######################################### 	
-#     CS 381 - Programming Lab #3		#
-#										#
-#  Noddy Lam						 	#
-#  lamno@oregonstate.edu               	#
-#										#
-#########################################
-
+require_relative "element"
+require_relative "player"
+require_relative "history"
 
 
 def player_choice(num)
